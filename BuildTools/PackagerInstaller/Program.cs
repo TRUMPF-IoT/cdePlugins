65f2c169c969789c86a6a424db42be2f0d27a77b
Markus Horstmann <markushorstmann@hotmail.com>
2020-05-28 15:54:16 -0700

    Use cdePackager NuGet instead of checked in tool

---
+++ BuildTools/PackagerInstaller/Program.cs
@@ -0,0 +1,15 @@
+﻿// SPDX-FileCopyrightText: 2020 Connectivity-Labs LLC
+// SPDX-License-Identifier: CC0-1.0
+
+using System;
+
+namespace PackagerInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
