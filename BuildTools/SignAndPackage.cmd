c5ec14801b8b266655869e5df67a1cbcebdc2846
Chris Muench <muenchris@outlook.com>
2020-09-21 10:09:23 -0400

    build tools check for packager availability

--- BuildTools/SignAndPackage.cmd
+++ BuildTools/SignAndPackage.cmd
@@ -2,5 +2,8 @@ rem <projectdir> <targetdir> <targetfilename> <cdePlatform - X64_V3 NETSTD_V20 e
 rem SPDX-FileCopyrightText: Copyright (c) 2009-2020 TRUMPF Laser GmbH, authors: C-Labs
 rem SPDX-License-Identifier: CC0-1.0
 call "%~dp0\signMeSha.bat" "%~2%~3" "%~dp0"
+if not exist "%~dp0cdePackager\cdePackager.exe" goto exit
+echo now packaging...
 "%~dp0cdePackager\cdePackager.exe" "%~2%~3" "%~2." "%~2." "%~4"
 if not %errorlevel%==0 goto :EOF
+:exit
