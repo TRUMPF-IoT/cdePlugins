2e619c73cc5f0c566e63b3a87abee55ae9a1e7cb
Chris Muench <chris@hyviva.com>
2025-07-27 13:35:43 -0700

    latest nugets Title Fix in Discord eventlog

--- src/178 - External Logger/CDMyLogger/ViewModel/TheDiscordLogger.cs
+++ src/178 - External Logger/CDMyLogger/ViewModel/TheDiscordLogger.cs
@@ -95,7 +95,7 @@ namespace CDMyLogger.ViewModel
                         var embed = new EmbedBuilder
                         {
                             Description = pItem.EventName,
-                            Title = pItem.EventString.Length>254 ? pItem.EventString.Substring(0,254) : pItem.EventString,
+                            Title = pItem.EventString==null ? pItem.EventName : (pItem.EventString.Length>254 ? pItem.EventString.Substring(0,254) : pItem.EventString),
                             Color = tCol,
                             Timestamp = pItem.EventTime,
                         };
